ef667b4cc6de8aa8f08c0a4c734575b64595b4bd
ruslansoma <rsoma@copyright.com>
2021-05-07 01:01:08 +0300

    RestAssured import add ExampleTest.java created with example test

---
+++ rest-api/src/main/java/test/ExampleTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.apache.http.HttpStatus.SC_OK;
+
+import org.junit.jupiter.api.Test;
+
+import io.restassured.RestAssured;
+
+public class ExampleTest {
+
+    @Test
+    public void retrieveRandomCatFactsTest() {
+        RestAssured.given().get("https://cat-fact.herokuapp.com/facts").prettyPeek().then().statusCode(SC_OK);
+    }
+
+}
